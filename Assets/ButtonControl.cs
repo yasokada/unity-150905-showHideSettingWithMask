f3a9cccd2c031ddea5b523217518f46f251afb33
y_okada <yasokada@gmail.com>
2015-09-05 11:17:54 +0900

    collapse toward the top/left

--- Assets/ButtonControl.cs
+++ Assets/ButtonControl.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 /*
+ * v0.2 2015/09/05
+ *  - collapse toward the left/top
  * v0.1 2015/09/05
  *  - show / hide setting
  */
@@ -19,9 +21,11 @@ public class ButtonControl : MonoBehaviour {
 		RectTransform rect = panel.GetComponent (typeof(RectTransform)) as RectTransform;
 		Vector2 size = rect.sizeDelta;
 		if (doShow) {
+			size.x = 200;
 			size.y = 250;
 		} else {
-			size.y = 5;
+			size.x = 20;
+			size.y = 20;
 		}
 		doShow = !doShow;
 		rect.sizeDelta = size;
