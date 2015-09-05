034f781249d2a1f22c9a7279b2687d4b8ab2a1af
y_okada <yasokada@gmail.com>
2015-09-05 11:01:25 +0900

    v0.1

---
+++ Assets/ButtonControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * v0.1 2015/09/05
+ *  - show / hide setting
+ */
+
+public class ButtonControl : MonoBehaviour {
+	private bool doShow = false;
+	
+	public void ShowSettingClick() {
+		GameObject panel = GameObject.Find ("PanelSetting");
+		if (panel == null) {
+			return;
+		}
+		
+		// size change
+		RectTransform rect = panel.GetComponent (typeof(RectTransform)) as RectTransform;
+		Vector2 size = rect.sizeDelta;
+		if (doShow) {
+			size.y = 250;
+		} else {
+			size.y = 5;
+		}
+		doShow = !doShow;
+		rect.sizeDelta = size;
+	}
+}
